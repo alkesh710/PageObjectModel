574f151f3d6e4da94b067a66cce357121ee61449
alkesh710 <40565133+alkesh710@users.noreply.github.com>
2018-12-11 06:05:13 -0800

    Update VerifyPageTitle.java

--- src/report/com/VerifyPageTitle.java
+++ src/report/com/VerifyPageTitle.java
@@ -11,7 +11,7 @@ import com.relevantcodes.extentreports.LogStatus;
 
 public class VerifyPageTitle {
 	
-	String url="http:www.facebook.com";
+	String url="http://www.facebook.com";
 	
 	@Test
 	public void verifyTitle()
